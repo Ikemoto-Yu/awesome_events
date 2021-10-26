d85851f4d40a85cfabc38b925d79c7918fa14c68
Ikemoto-Yu <cosper79@gmail.com>
2021-10-26 16:27:25 +0900

    ユーザーのモデルを作成する

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use Omniauth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
   else
