391ba88b9f88341888bd06574d7f285c9c9d0758
Ikemoto-Yu <cosper79@gmail.com>
2021-11-06 00:59:45 +0900

    修正２

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
+    provider :github, "6438275f4ef5f011dab1", "5c23f918ad775ad018d85f99cc6497d7f04483e3"
   else
     provider :github
       Rails.application.credentials.github[:client_id]
